afd3d903c9f2fcb4f783afa3e226565407e4209e
callmeteddy17 <dtd172000@gmail.com>
2023-03-01 00:00:12 +0700

    update traffic light

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import ApplyTasks from './component/ApplyTasks';
 import CountTasks from './component/CountTasks';
 import Input from './component/Input';
+import TrafficLight from './component/TrafficLight';
 
 function App() {
     const [job, setJob] = useState('');
@@ -10,6 +11,7 @@ function App() {
 
     return (
         <div className="main">
+            <TrafficLight />
             <Input job={job} setJob={setJob} setJobs={setJobs} />
             <ApplyTasks jobs={jobs} />
             <CountTasks jobs={jobs} />
